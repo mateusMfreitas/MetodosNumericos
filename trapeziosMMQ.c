a2add42597a0d3323925e7dc62361339f69de603
tamitavares <tamiris.lemes18@gmail.com>
2022-05-24 09:36:57 -0300

    tentativa de polinomio

--- trapeziosMMQ.c
+++ trapeziosMMQ.c
@@ -9,9 +9,11 @@ void aloca(float **p, int tam);
 // Trapézios
 
 float calculaH(float minimo, float maximo, int nroDivisoes);
-void valoresx(float *x, float h, float maximo, float minimo, int nroDivisoes);
+void valoresx(float *fx, float h, float maximo, float minimo, int nroDivisoes, int grau, float *coef);
 void valoresfx(float *x, float *fx, float *coef, float maximo, float minimo, int grau, int nroDivisoes);
 float calculaIt(float h, float minimo, float maximo);
+
+
 
 int main()
 {
@@ -70,12 +72,12 @@ int main()
 				scanf("%f", coeficiente + i);
 				fflush(stdin);
 			}
-			
+
 			h = calculaH(minimo, maximo, nroDivisoes);
 			printf("\nH = %.2f\n", h);
-			valoresx(valoresX, h, maximo, minimo, nroDivisoes);
-			printf("\nTESTEEE\n"); 
-			valoresfx(valoresX, valoresFx, coeficiente, maximo, minimo, grau, nroDivisoes);
+			valoresx(valoresX, h, maximo, minimo, nroDivisoes, grau, coeficiente);
+			printf("\nTESTEEE\n");
+			// valoresfx(valoresX, valoresFx, coeficiente, maximo, minimo, grau, nroDivisoes);
 
 			system("pause");
 			break;
@@ -116,54 +118,68 @@ float calculaH(float minimo, float maximo, int nroDivisoes)
 }*/
 //		lista de valores de x, o passo entre as divisões, e o valor, int nroDivisoes máximo e mínimo
 
-void valoresx(float *x, float h, float maximo, float minimo, int nroDivisoes)
-{
-	int i;
-	int total = nroDivisoes - 1; // tirar maximo
-	printf("\nX[%i] = %.2f", 0, minimo);
-	for (i = 0; i < total; i++)
-	{
-		aloca(&x, i + 1);
-		*(x + i) = (minimo + (h * (i + 1)));
-		printf("\nX[%i] = %.2f", i + 1, *(x + i));
-	}
-	printf("\nX[%i] = %.2f\n\n", nroDivisoes, maximo);
-} // valoresx
-
-void valoresfx(float *x, float *fx, float *coef, float maximo, float minimo, int grau, int nroDivisoes)
+void valoresx(float *fx, float h, float maximo, float minimo, int nroDivisoes, int grau, float *coef)
 {
 	int i, j;
-	float valorF = 0, teste=2;
-
-for(i=0; i<nroDivisoes; i++){  //i relaciona com x
-		valorF=0;
-		for (j=0; j<=grau; j++)  //j relaciona com o polinomio
+	int total = nroDivisoes; // tirar maximo
+	float valorF = 0;
+	float x;
+	// printf("\nX[%i] = %.2f", 0, minimo);
+	for (i = 0; i < total; i++)
+	{
+		aloca(&fx, i + 1);
+		x = 0;
+		valorF = 0;
+		x = (minimo + (h * (i + 1)));
+		for (j = 0; j <= grau; j++)
 		{
-			if(i==0)
-				valorF = (*(coef+j))*(pow(minimo,j));
-			else if(i==nroDivisoes)
-				valorF+= (*(coef+j))*(pow(minimo,j));
+			if (i == 0)
+				valorF = (*(coef + j)) * (pow(minimo, j));
+			else if (i == nroDivisoes)
+				valorF += (*(coef + j)) * (pow(maximo, j));
 			else
-				valorF+=(*(coef+j))*(pow(*(x+i),j));
-
-		} // j
-		aloca(&fx, i+1);
-		*(fx+i) = valorF;
-		printf("\nF(%.2f)= %.2f", *(x + i), *(fx + i));
-}//i
-} // valoresfx
-
-/*
-void calculaFx(float *fx, float *x, int grau){
-	int i;
-    float valorF = 0;
-	for(i=0; i<=grau; i++){
-        valorF +=(*(coef+i))*(pow(*(x+i),i));		
+				valorF += (*(coef + j)) * (pow(x, j));
+		}
+		printf("\nFx[%i] = %.2f", i + 1, *(fx + i));
 	}
-	return valorFa;
-}
-*/
-
+	// printf("\nX[%i] = %.2f\n\n", nroDivisoes, maximo);
+} // valoresx
+  /*
+  void valoresfx(float *x, float *fx, float *coef, float maximo, float minimo, int grau, int nroDivisoes)
+  {
+	  int i, j;
+	  float valorF = 0, teste = 2;
+  
+	  for (i = 0; i < nroDivisoes; i++)
+	  { // i relaciona com x
+		  valorF = 0;
+		  for (j = 0; j <= grau; j++) // j relaciona com o polinomio
+		  {
+			  if (i == 0)
+				  valorF = ( *(coef + j)) * (pow(minimo, j));
+			  else if (i == nroDivisoes)
+				  valorF += (*(coef + j)) * (pow(minimo, j));
+			   else
+				  valorF+=(*(coef+j))*(pow(*(x+i),j));
+			  printf("\nvalor F = %.2f", &valorF);
+		  } // j
+		  aloca(&fx, i + 1);
+		  *(fx + i) = valorF;
+		  printf("\n valor de f: %f", *(fx + i));
+		  // printf("\nF(%.2f)= %.2f", *(fx), *(fx + i));
+	  } // i
+  } // valoresfx
+  */
+  /*
+  void calculaFx(float *fx, float *x, int grau){
+	  int i;
+	  float valorF = 0;
+	  for(i=0; i<=grau; i++){
+		  valorF +=(*(coef+i))*(pow(*(x+i),i));
+	  }
+	  return valorFa;
+  }
+  */
 
 /*
 float calculaFx ()
